9cdc0a2d442a79c9a03fa32a76386924aa193332
SurjeetMandal <surjeetmandal698@gmail.com>
2024-02-18 19:12:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 function daysOfAYear(year) {
-	if (year%4 === 0) {
-		return 366;
-	}
-	else if (year%400 === 0) {
-		return 366;
-	}
-	else if (year%100 === 0) {
+	if ((year%4 === 0 && year%100 !=0) || (year%400 === 0)) {
 		return 366;
 	} 
 	else {
